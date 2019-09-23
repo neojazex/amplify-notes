1266f24debc54245c9e336b0ea808fe9852ba988
jason <jason@vintrace.com>
2019-09-24 08:05:36 +1000

    Notes add - add, view, delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Amplify, { API, graphqlOperation} from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import { listNotes } from './graphql/queries';
+import { createNote, deleteNote } from './graphql/mutations';
 
-function App() {
+Amplify.configure(aws_exports);
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
+
+  useEffect(() => {
+    handleListNotes();
+
+  }, []);
+
+  const handleListNotes = async () => {
+    const { data } = await API.graphql(graphqlOperation(listNotes));
+    setNotes(data.listNotes.items);
+  }
+
+  const handleAddNote = async event => {
+    event.preventDefault();
+    const temp = { note };
+    const { data } = await API.graphql(graphqlOperation(createNote, { input: temp}));
+    const newNote = data.createNote;
+    const updatedNotes = [newNote, ...notes ];
+    setNotes(updatedNotes);
+    setNote("");
+  }
+
+  const handleDeleteNote = async id => {
+    const temp = { id };
+    const { data } = await API.graphql(graphqlOperation(deleteNote, { input: temp}));
+    const deletedNoteId = data.deleteNote.id;
+    const deletedNoteIdx = notes.findIndex(note => note.id === deletedNoteId);
+    const updatedNotes = [...notes.slice(0, deletedNoteIdx), ...notes.slice(deletedNoteIdx + 1)];
+    setNotes(updatedNotes);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column items-center justify-center bg-washed-red pa3">
+      <h1 className="courier f2">
+        Amplify Notes
+      </h1>
+      {/* note form */}
+
+      <form className="mb3" onSubmit={handleAddNote}>
+        <input type="text" placeholder="Enter note" className="pa2 f4" value={note} onChange={({ target }) => setNote(target.value)}/>
+        <button type="submit" className="pa2 f4">Add</button>
+      </form>
+
+      {/* note list */}
+      <div>
+        {notes.map((item, i) => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4" onClick={() => handleDeleteNote(item.id)}><span>&times;</span></button>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
